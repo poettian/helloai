5c2f4f12058d2789c305ce06274c7bb085276d80
tianzhiwei <poettian@gmail.com>
2025-03-26 19:13:45 +0800

    update

--- hello.py
+++ hello.py
@@ -1,4 +1,81 @@
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-llm = ChatOpenAI()
-print(llm.invoke("Hello, world!"))
+model = ChatGoogleGenerativeAI(model="gemini-2.5-pro-exp-03-25")
+
+from typing import Literal
+
+from langchain_core.tools import tool
+
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import MessagesState, StateGraph, START, END
+from langgraph.prebuilt import ToolNode
+
+memory = MemorySaver()
+
+
+@tool
+def search(query: str):
+    """Call to surf the web."""
+    # This is a placeholder for the actual implementation
+    # Don't let the LLM know this though 😊
+    return "It's sunny in San Francisco, but you better look out if you're a Gemini 😈."
+
+
+tools = [search]
+tool_node = ToolNode(tools)
+bound_model = model.bind_tools(tools)
+
+
+def should_continue(state: MessagesState):
+    """Return the next node to execute."""
+    last_message = state["messages"][-1]
+    # If there is no function call, then we finish
+    if not last_message.tool_calls:
+        return END
+    # Otherwise if there is, we continue
+    return "action"
+
+
+def filter_messages(messages: list):
+    # This is very simple helper function which only ever uses the last message
+    return messages[-1:]
+
+
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    messages = filter_messages(state["messages"])
+    response = bound_model.invoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": response}
+
+
+# Define a new graph
+workflow = StateGraph(MessagesState)
+
+# Define the two nodes we will cycle between
+workflow.add_node("agent", call_model)
+workflow.add_node("action", tool_node)
+
+# Set the entrypoint as `agent`
+# This means that this node is the first one called
+workflow.add_edge(START, "agent")
+
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    # First, we define the start node. We use `agent`.
+    # This means these are the edges taken after the `agent` node is called.
+    "agent",
+    # Next, we pass in the function that will determine which node is called next.
+    should_continue,
+    # Next, we pass in the pathmap - all the possible nodes this edge could go to
+    ["action", END],
+)
+
+# We now add a normal edge from `tools` to `agent`.
+# This means that after `tools` is called, `agent` node is called next.
+workflow.add_edge("action", "agent")
+
+# Finally, we compile it!
+# This compiles it into a LangChain Runnable,
+# meaning you can use it as you would any other runnable
+app = workflow.compile(checkpointer=memory)
